9db711e88b21a93cc054a9085a2fc9e0e50e7309
Ankushjare <jareankush@gmail.com>
2021-10-24 20:17:11 +0530

    compare to Method

--- Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -7,13 +7,14 @@ namespace Line_Comparison_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
+            Console.WriteLine("Enter the value First of (x1,y1),(x2,y2) cordinates\n");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
             int length1 = toFindLength(x1, y1, x2, y2);
 
+            Console.WriteLine("Enter the value Second of (x1,y1),(x2,y2) cordinates\n");
             x1 = Convert.ToInt32(Console.ReadLine());
             x2 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
@@ -21,6 +22,9 @@ namespace Line_Comparison_Problem
             int length2 = toFindLength(x1, y1, x2, y2);
 
             Console.WriteLine(length1.Equals(length2));
+
+            Console.WriteLine("Return value (comparison) =" + length1.CompareTo(length2));
+
         }
         private static int toFindLength(int x1, int y1, int x2, int y2)
         {
